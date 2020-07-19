92ef80a02f22cb711bfc0f32fa780088da1a3de1
AlexDefoy <alex.defoy@umontreal.ca>
2020-07-19 13:47:05 -0400

    Enlevé les diagrammes superflus et le dossier javaProject. Changements mineurs au rapport.htm.

--- devoir_2/Implementation/javaProject/CentreDeDonnees/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-		Controleur controleur = new Controleur();
-		InterfaceUtilisateur.controleur = controleur;
-		controleur.start();
-    }
-}
